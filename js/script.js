73f1e1bd4ba6724d6a0dd0cef740bb017d5c117a
Vivek Saliya <viveksaliya004@gmail.com>
2024-07-05 22:01:26 +0530

    add about & projects section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 window.onscroll = function () {
   var header = document.getElementById("header");
-  if (window.pageYOffset > 80) {
+  if (window.pageYOffset > 100) {
     header.style.top = "30px";
   } else {
-    header.style.top = "-60px";
+    header.style.top = "-100px";
   }
 };
